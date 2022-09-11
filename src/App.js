7fadcde900572c1d58d88677e3f614a4ad05e8b3
Keyukemi <keyukemi.ku@gmail.com>
2022-09-11 17:00:51 +0100

    add particle.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import ParticlesBackground
+ from './components/particlesbg/particlesbg';
 // import FaceRecognition from './components/face-recognition/facerecognition';
 import ImageLinkForm from './components/image-link-form/imagelinkform';
 import Logo from './components/logo/logo';
@@ -8,6 +10,7 @@ import Rank from './components/rank-file/rank';
 function App() {
   return (
     <div className="App">
+      <ParticlesBackground />
       <Navigation />
       <Logo />
       <Rank/>
