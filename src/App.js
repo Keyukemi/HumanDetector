511e0151c19d24097b78ac6aae91793145f33b1b
Keyukemi <keyukemi.ku@gmail.com>
2022-10-23 19:22:49 +0100

    moved clarifai AP

--- src/App.js
+++ src/App.js
@@ -11,24 +11,24 @@ import Rank from './components/rank-file/rank';
 import { Component } from 'react';
 
 
-
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'Signin',
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: ""
+  }
+}
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'Signin',
-      isSignedIn: false,
-      user: {
-        id: "",
-        name: "",
-        email: "",
-        entries: 0,
-        joined: ""
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -69,43 +69,13 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-
-
-    const USER_ID = 'vtszy7uwboz9';
-    // Your PAT (Personal Access Token) can be found in the portal under Authentification
-    const PAT = 'dbee67e7b6f0464e8c005566a64f1f1c';
-    const APP_ID = 'cf0200675a43489cb1d54999f2206da5';
-    // Change these to whatever model and image URL you want to use
-    const MODEL_ID = 'face-detection';
-    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
-    const IMAGE_URL = this.state.input;
-
-    const raw = JSON.stringify({
-      "user_app_id": {
-        "user_id": USER_ID,
-        "app_id": APP_ID
-      },
-      "inputs": [
-        {
-          "data": {
-            "image": {
-              "url": IMAGE_URL
-            }
-          }
-        }
-      ]
-    });
-
-    const requestOptions = {
+    fetch("http://localhost:3000/imageurl", {
       method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Key ' + PAT
-      },
-      body: raw
-    };
-
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        image_url: this.state.input
+      })
+    })
       .then(response => response.json())
       .then(response => {
         if (response) {
@@ -120,6 +90,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }))
             })
+            .catch(console.log)
         }
         this.showFaceBox(this.calcFaceLoc(response))
       })
@@ -128,7 +99,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false })
+      this.setState(initialState)
     }
     else if (route === 'home') {
       this.setState({ isSignedIn: true })
