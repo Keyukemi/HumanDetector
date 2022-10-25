44a9b2de237eb6b9ef6653308a7d517b6e67acde
Keyukemi <keyukemi.ku@gmail.com>
2022-10-25 21:20:08 +0100

    update backend connections

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://nameless-cliffs-78176.herokuapp.com/imageurl", {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -79,7 +79,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://nameless-cliffs-78176.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
